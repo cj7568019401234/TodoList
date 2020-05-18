130cefd94348909f3874fca038015bba260eebe3
liucaijun <liucaijun@jd.com>
2020-05-18 21:34:04 +0800

    simplify css

--- js/ToDoList-v2.js
+++ js/ToDoList-v2.js
@@ -1,75 +1,55 @@
-(function(){
-    var addBtn = document.querySelector('#add-btn');//点击添加任务
-    var onList = document.querySelector('.on-list');//待完成的任务列表
-    var endList = document.querySelector('.end-list');//已完成的任务列表
-    var onTaskNum = document.querySelector('.on-task-num');//待完成的任务数量
-    var endTaskNum = document.querySelector('.end-task-num');//已完成的任务数量
-    var mainDiv = document.querySelector('.main-div');//任务区域，包括待完成任务、已完成任务
-    
+(function () {
+    const addBtn = document.querySelector('.nav__add');//点击添加任务
+    const todoList = document.querySelector('.task--todo__list');//待完成的任务列表
+    const doneList = document.querySelector('.task--done__list');//已完成的任务列表
+    const onTaskNum = document.querySelector('.task--todo__num');//待完成的任务数量
+    const endTaskNum = document.querySelector('.task--done__num');//已完成的任务数量
+    const task = document.querySelector('.task');//任务区域，包括待完成任务、已完成任务
+
     /**
-     * 添加任务
+     * 导航栏上的添加按钮添加点击事件
      */
-    function addTask(){
-        var inputText = document.querySelector('.input-task').value.replace(/(^\s*)|(\s*$)/g,"");//去除任务前后的空格
-        if(inputText.length<=0 ){//如果输入空格就不添加任务
-            return
-        }
-        var checkBox = document.createElement('input');//标记是否完成任务的checkbox
-        checkBox.setAttribute('type','checkbox');
-        checkBox.classList.add('check','center');
+    addBtn.onclick = function () {
+        var inputText = document.querySelector('.nav__task').value.trim();//去除任务前后的空格
+        if (!inputText.length) return
 
-        var textNode = document.createElement('label');//任务文本
-        textNode.innerHTML = inputText;
-        textNode.classList.add('task-text');
-        var button = document.createElement('div');//删除按钮
-        button.innerHTML = 'DEL';
-        button.classList.add('del-button');
-
-        var line = document.createElement('div');//任务分割线
-        line.classList.add('new-line');
-
-        var newTask = document.createElement('div');
-        newTask.classList.add('new-task');
-
-        newTask.appendChild(checkBox);
-        newTask.appendChild(textNode);
-        newTask.appendChild(button);
-        newTask.appendChild(line);
-        onList.appendChild(newTask);
-        onTaskNum.innerHTML = parseInt(onTaskNum.innerHTML) + 1;//添加任务的时候，待完成任务数+1
-    }
-
-    addBtn.onclick = function(){//导航栏上的添加按钮添加点击事件
-        addTask();
+        var childNode = `<div class="task__div">
+        <input type="checkbox" class="task__check">
+        <label class="task__text">${inputText}</label>
+        <div class="task__delBtn">DEL</div>
+        <div class="task__line--other"></div>
+        </div>
+        `
+        todoList.innerHTML += childNode;
+        onTaskNum.innerText = parseInt(onTaskNum.innerText) + 1;//添加任务的时候，待完成任务数+1
     }
 
     /**
      * 设置事件委托
      */
-    mainDiv.addEventListener('click',function(e){
-        // console.log(e)
+    task.addEventListener('click', function (e) {
         var crrTask = e.target.parentNode;
 
         //点击checkbox按钮
-        if(e.target.nodeName.toLowerCase() === 'input'){   
-            if(e.target.checked){//已勾选的是已完成任务
-                onList.removeChild(crrTask);
-                endList.appendChild(crrTask); 
-                endTaskNum.innerHTML = parseInt(endTaskNum.innerHTML) + 1; 
-                onTaskNum.innerHTML = parseInt(onTaskNum.innerHTML) - 1;  
-            }else{//未勾选的是未完成任务
-                endList.removeChild(crrTask);
-                onList.appendChild(crrTask);
-                onTaskNum.innerHTML = parseInt(onTaskNum.innerHTML) + 1; 
-                endTaskNum.innerHTML = parseInt(endTaskNum.innerHTML) - 1;
+        if (e.target.nodeName.toLowerCase() === 'input') {
+            if (e.target.checked) {//已勾选的是已完成任务
+                todoList.removeChild(crrTask);
+                doneList.appendChild(crrTask);
+                endTaskNum.innerText = parseInt(endTaskNum.innerText) + 1;
+                onTaskNum.innerText = parseInt(onTaskNum.innerText) - 1;
+            } else {//未勾选的是未完成任务
+                doneList.removeChild(crrTask);
+                todoList.appendChild(crrTask);
+                onTaskNum.innerText = parseInt(onTaskNum.innerText) + 1;
+                endTaskNum.innerText = parseInt(endTaskNum.innerText) - 1;
             }
-        }else if(e.target.classList.contains('del-button')){//点击DEL删除按钮
-            if(e.target.parentNode.parentNode.classList.contains('end-list')){//已完成的任务
-                endList.removeChild(crrTask);
-                endTaskNum.innerHTML = parseInt(endTaskNum.innerHTML) - 1;  
-            }else{//是待完成任务
-                onList.removeChild(crrTask);
-                onTaskNum.innerHTML = parseInt(onTaskNum.innerHTML) - 1;
+        } else if (e.target.classList.contains('del-button')) {//点击DEL删除按钮
+            if (e.target.parentNode.parentNode.classList.contains('doneList')) {//已完成的任务
+                doneList.removeChild(crrTask);
+                endTaskNum.innerText = parseInt(endTaskNum.innerText) - 1;
+            } else {//是待完成任务
+                todoList.removeChild(crrTask);
+                onTaskNum.innerText = parseInt(onTaskNum.innerText) - 1;
             }
         }
     })
