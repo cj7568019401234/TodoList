2cc1279745650bda87a75f13bd11c6be53dad52b
liucaijun <liucaijun@jd.com>
2020-05-20 22:08:09 +0800

    use React

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Task from './task';
+import TaskList from './task';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Task />
+    <TaskList />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- my-app/src/task.js
+++ my-app/src/task.js
@@ -1,65 +1,90 @@
 import React from 'react';
 import './index.css';
 
-class Task extends React.Component {
+/**
+ * 任务
+ * @param {props.checkTyp}  任务类型：todo为待完成；done为已完成
+ * @param {props.btnType}  按钮类型：del-todo为删除待完成任务；del-done为删除已完成任务
+ * @param {props.checked}  checkbox类型：待完成任务不勾选；已完成任务勾选
+ * @param {props.taskList}  任务列表
+ */
+function Task(props) {
+    return (
+        props.taskList.map(function (task,index) {
+             return <div className="task__div" key={index+props.checkType}>
+                <input type="checkbox" className="task__check" data-type={props.checkType} checked={props.checked} />
+                <label className="task__text">{task}</label>
+                <div className="task__delBtn" data-type={props.btnType}>DEL</div>
+                <div className="task__line--other"></div>
+            </div>
+        })
+    );
+}
+
+class TaskList extends React.Component {
     constructor(props) {
         super(props);
         this.inputText = React.createRef();
-
+        this.taskText = React.createRef();
         this.state = {
             todoList: [],
-            doneList: [],
-            text : 2
-            // inputText : React.createRef()
+            doneList: []
         }
         this.addTask = this.addTask.bind(this);
         this.alterTask = this.alterTask.bind(this);
     }
 
+    /**
+     * 添加新任务
+     */
     addTask() {
         const text = this.inputText.current.value;
+        if(!text.trim()) return; //不允许添加空白任务
+
+        this.inputText.current.value = ''; //添加任务后清除输入框
         this.setState({
-            todoList: this.state.todoList.concat(text)
+            todoList: this.state.todoList.concat(text)  
         });
     };
 
+    /**
+     * 勾选任务状态或删除任务
+     * @param {event}  发生点击事件的Event对象
+     */
+    alterTask(event) {
+        if (!event.target.dataset.type) return;
+        const parent = event.target.parentNode;
+        const text = parent.getElementsByClassName("task__text")[0].innerText;
 
-    alterTask(e) {
-        const todoList = document.querySelector('.task--todo__list');// 待完成的任务列表
-        const doneList = document.querySelector('.task--done__list');// 已完成的任务列表
-        const onTaskNum = document.querySelector('.task--todo__num');// 待完成的任务数量
-        const endTaskNum = document.querySelector('.task--done__num');// 已完成的任务数量
-        const crrTask = e.target.parentNode;
+        var todo = this.state.todoList;
+        var done = this.state.doneList;
+        const type = event.target.dataset.type;
 
-        // 点击checkbox按钮
-        if (e.target.classList.contains('task__check')) {
-            if (e.target.checked) { // 已勾选的是已完成任务
-                crrTask.dataset.type = 'done';
-                todoList.removeChild(crrTask);
-                doneList.appendChild(crrTask);
-                endTaskNum.innerText = parseInt(endTaskNum.innerText, 10) + 1;
-                onTaskNum.innerText = parseInt(onTaskNum.innerText, 10) - 1;
-            } else { // 未勾选的是未完成任务
-                crrTask.dataset.type = 'todo';
-                doneList.removeChild(crrTask);
-                todoList.appendChild(crrTask);
-                onTaskNum.innerText = parseInt(onTaskNum.innerText, 10) + 1;
-                endTaskNum.innerText = parseInt(endTaskNum.innerText, 10) - 1;
+        // 点击按钮
+        if (type === 'todo' || type === 'del-todo') { //勾选或删除未完成任务
+            const index = this.state.todoList.indexOf(text);
+            this.state.todoList.splice(index, 1);
+            todo = this.state.todoList
+            if (type === 'todo') { //勾选未完成任务，需要把当前任务添加到已完成列表中
+                done = this.state.doneList.concat(text);
             }
-        } else if (e.target.classList.contains('task__delBtn')) { // 点击DEL删除按钮
-            if (crrTask.dataset.type === 'done') { // 已完成的任务
-                doneList.removeChild(crrTask);
-                endTaskNum.innerText = parseInt(endTaskNum.innerText, 10) - 1;
-            } else { // 是待完成任务
-                todoList.removeChild(crrTask);
-                onTaskNum.innerText = parseInt(onTaskNum.innerText, 10) - 1;
+        } else if (type === 'done' || type === 'del-done') { //勾选或删除已完成任务
+            const index = this.state.doneList.indexOf(text);
+            this.state.doneList.splice(index, 1);
+            done = this.state.doneList
+            if (type === 'done') { //勾选已完成任务，需要把当前任务添加到未完成列表中
+                todo = this.state.todoList.concat(text);
             }
-        }
+        } else return;
+
+        this.setState({
+            todoList: todo,
+            doneList: done
+        })
     };
 
     render() {
-        console.log('render',this.state.todoList);
-        var taskList = this.state.todoList;
+        const { todoList, doneList } = this.state;
 
         return (
             <div>
@@ -68,31 +93,34 @@ class Task extends React.Component {
                     <input className='nav__task' type='text' placeholder='添加任务' ref={this.inputText} />
                     <div className='nav__add' onClick={() => this.addTask()}>添加</div>
                 </nav>
-                <div className='task' onClick={() => this.alterTask()}>
+                <div className='task' onClick={this.alterTask}>
                     <div className='task--todo'>
                         <h3>待完成
-                <span className='task--todo__num'>0</span>
+                            <span className='task--todo__num'>{todoList.length}</span>
                         </h3>
                         <div className='task__line--first'></div>
                         <div className='task--todo__list'>
-                            {
-                                taskList.map(function (task) {
-                                    return <div className="task__div" data-type="todo">
-                                        <input type="checkbox" className="task__check" />
-                                        <label className="task__text">{task}</label>
-                                        <div className="task__delBtn">DEL</div>
-                                        <div className="task__line--other"></div>
-                                    </div>
-                                })
-                            }
+                            <Task
+                                checkType='todo'
+                                btnType='del-todo'
+                                checked=''
+                                taskList={todoList}
+                            />
                         </div>
                     </div>
                     <div className='task--done'>
                         <h3>已完成
-                <span className='task--done__num'>0</span>
+                <span className='task--done__num'>{doneList.length}</span>
                         </h3>
                         <div className='task__line--first'></div>
-                        <div className='task--done__list'></div>
+                        <div className='task--done__list'>
+                            <Task
+                                checkType='done'
+                                btnType='del-done'
+                                checked='checked'
+                                taskList={doneList}
+                            />
+                        </div>
                     </div>
                 </div>
                 <footer>
@@ -103,4 +131,4 @@ class Task extends React.Component {
     }
 }
 
-export default Task;
+export default TaskList;
