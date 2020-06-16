cac6549c3cbef6a6a650e262bd4df2cc9bffc69e
liucaijun1 <liucaijun@jd.com>
2020-06-16 09:01:24 +0800

    feat:add index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import TodoApp from './pages/todos/index';
+// import { Card, Col, Row } from 'Antd';
+import 'antd/dist/antd.css'
 import store from './store/index.js'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
+    {/* <div className="site-card-wrapper">
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>
+            Card content
+        </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>
+            Card content
+        </Card>
+        </Col>
+      </Row>
+    </div> */}
     <TodoApp />
   </Provider>,
   document.getElementById('root')
