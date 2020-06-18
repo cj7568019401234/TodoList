0b2534aff54ebd15b88202906b7810ca48c46269
liucaijun1 <liucaijun@jd.com>
2020-06-18 08:15:25 +0800

    show home page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route } from 'react-router'
-import Apps from './pages/index/index';
+import { BrowserRouter } from 'react-router-dom';
+import { Router, Route, Link, IndexRoute } from 'react-router'
 import TodoApp from './pages/todos/index'
+// import { Router, Route } from 'react-router'
+import Apps from './pages/index/index';
 import 'antd/dist/antd.css'
 import store from './store/index.js'
 // import { IndexRoute } from 'react-router'
@@ -11,15 +13,10 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-    <Route path="/" component={Apps}>
-      {/* 当 url 为/时渲染 Dashboard */}
-      {/* <IndexRoute component={Dashboard} /> */}
-      <Route path="todo" component={TodoApp} />
-      <Route path="account" component={TodoApp}>
-      </Route>
-    </Route>
-  </Router>
+    <BrowserRouter>
+      <Apps />
+
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, Layout, Avatar } from 'antd';
 // import { Router, Route, Link } from 'react-router'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import TodoApp from '../todos/index'
 import '../todos/index.css';
 import './style/index.css'
 import todoIcon from './style/todo.png';
@@ -12,34 +13,43 @@ const { Meta } = Card;
 
 function Apps() {
     return (
-        <Layout>
-            <Header className='nav'>
-                <label className='nav__index-logo'>❤ Home Page</label>
-            </Header>
-            <div className="container">
-                <Link to='/todo'>
-                <div className="site-card-wrapper">
-                    <Card style={{ width: 300 }}>
-                        <Meta
-                            avatar={<Avatar src={todoIcon} />}
-                            title="TodoList"
-                            description="可添加、删除、修改待办任务"
-                        />
-                    </Card>
-                    <Card style={{ width: 300}}>
-                        <Meta
-                            avatar={<Avatar src={accountIcon} />}
-                            title="Account book"
-                            description="多身份记账账本"
-                        />
-                    </Card>
+        <Router>
+            <Layout>
+                <Header className='nav'>
+                    <label className='nav__index-logo'>❤ Home Page</label>
+                </Header>
+                <div className="container">
+                    <div className="site-card-wrapper">
+                        <Link to='/todo'>
+                            <Card style={{ width: 300 }}>
+                                <Meta
+                                    avatar={<Avatar src={todoIcon} />}
+                                    title="TodoList"
+                                    description="可添加、删除、修改待办任务"
+                                />
+                            </Card>
+                        </Link>
+                        <Card style={{ width: 300 }}>
+                            <Meta
+                                avatar={<Avatar src={accountIcon} />}
+                                title="Account book"
+                                description="多身份记账账本"
+                            />
+                        </Card>
+                    </div>
                 </div>
-                </Link>
-            </div>
-            <footer>
-                <p>Copyright © 2020 TodoList. Posted by : Cj</p>
-            </footer>
-        </Layout>
+                <footer>
+                    <p>Copyright © 2020 TodoList. Posted by : Cj</p>
+                </footer>
+            </Layout>
+
+            <Route path="/" component={Apps}>
+                {/* <IndexRoute component={Apps} />  */}
+                <Route path="todo" component={TodoApp} />
+                <Route path="account" component={TodoApp}>
+                </Route>
+            </Route>
+        </Router>
     );
 }
 
