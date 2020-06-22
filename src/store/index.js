04a4aee8a56bcba9ab2e9882fa1471ce6636bbb1
liucaijun <liucaijun@jd.com>
2020-06-22 21:40:27 +0800

    feat:connect MongoDB

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
 import { reducer } from './todos/index'
 import { createStore } from 'redux';
+const TodoDB = require('./todos/DataAccess');
+const todoDB = new TodoDB();
+
 
 const initState = {
-    todoList: []
+    todoList: todoDB.findTodos(),
 }
 
 export default createStore(reducer, initState)
--- src/store/todos/DataAccess.js
+++ src/store/todos/DataAccess.js
@@ -1,30 +1,148 @@
 'use strict';
-const MongoClient = require('mongodb').MongoClient;
-class DbConnection {
 
-    constructor(connectionUri) {
-        this.Uri = connectionUri;
+const ObjectID = require('mongodb').ObjectID;
+const DbConnection = require('../DbConnection');
+
+const collection = 'TodoList';
+
+const connect = () => new DbConnection('mongodb://127.0.0.1:27017/TodoList');
+
+const filters = {
+    id: (id) => {
+        return { _id: new ObjectID(id) };
+    },
+    tag: (tag) => {
+        return { tags: { $regex: new RegExp(tag, 'i') } };
+    },
+    title: (title) => {
+        return { 'title': { $regex: new RegExp(title, 'i') } };
     }
-    
-    open() {
+};
+
+class TodoDB {
+    addTodo(note) {
+        const connection = connect();
+
         return new Promise((resolve, reject) => {
-            MongoClient.connect(this.Uri)
-                .then(db => {
-                    this.Db = db;
-                    resolve();
+            connection
+                .open()
+                .then(() => {
+                    connection.Db.collection(collection)
+                        .findOne(filters.title(note.title))
+                        .then(noteData => {
+                            if (noteData) {
+                                connection.close();
+                                reject(Error('Note already exists'));
+                            } else {
+                                connection.Db
+                                    .collection(collection)
+                                    .insertOne(note)
+                                    .then(result => {
+                                        connection.close();
+                                        resolve({ id: result.insertedId });
+                                    })
+                                    .catch(error => {
+                                        connection.close();
+                                        reject(error);
+                                    });
+                            }
+                        })
+                        .catch(error => {
+                            connection.close();
+                            reject(error);
+                        });
                 })
                 .catch(error => {
-                    console.log(error);
-                    reject();
+                    reject(error);
+                    connection.close();
                 });
         });
     }
 
-    close() {
-        if (this.Db) {
-            this.Db.close().catch(error => console.log(error));
-        }
+    removeTodo(id) {
+        const connection = connect();
+
+        return new Promise((resolve, reject) => {
+            connection
+                .open()
+                .then(() => {
+                    connection.Db
+                        .collection(collection)
+                        .findOneAndDelete(filters.id(id))
+                        .then(() => {
+                            resolve();
+                            connection.close();
+                        })
+                        .catch(error => {
+                            reject(error);
+                            connection.close();
+                        });
+                })
+                .catch(error => {
+                    resolve(error);
+                    connection.close();
+                });
+        });
+    }
+
+    updateTodo(id, note) {
+        const connection = connect();
+
+        return new Promise((resolve, reject) => {
+            connection
+                .open()
+                .then(() => {
+                    connection.Db
+                        .collection(collection)
+                        .update(
+                            filters.id(id),
+                            {
+                                $set: {
+                                    title: note.title,
+                                    content: note.content,
+                                    tags: note.tags,
+                                    updated_date: note.updated_date
+                                }
+                            })
+                        .then(() => {
+                            resolve();
+                            connection.close();
+                        })
+                        .catch(error => {
+                            reject(error);
+                            connection.close();
+                        });
+                })
+                .catch(error => {
+                    resolve(error);
+                    connection.close();
+                });
+        });
+    }
+
+    findTodos() {
+        const connection = connect();
+
+        return new Promise((resolve, reject) => {
+            connection
+                .open()
+                .then(() => {
+                    connection.Db.collection(collection)
+                        .then(todos => {
+                            resolve(todos);
+                            connection.close();
+                        })
+                        .catch(error => {
+                            reject(error);
+                            connection.close();
+                        });
+                })
+                .catch(error => {
+                    reject(error);
+                    connection.close();
+                });
+        });
     }
 }
 
-module.exports = DbConnection;
+module.exports = TodoDB;
